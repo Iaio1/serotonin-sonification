0b6c841348feb4ca6e9344a914d86f22e1a2d476
Pablo <pabloprietoroca@gmail.com>
2025-06-23 22:28:45 +0200

    Added methd to export peak_amplitudes and their positions on the IT

--- core/output_manager.py
+++ core/output_manager.py
@@ -5,16 +5,16 @@ import pandas as pd
 
 class OutputManager:
     @staticmethod
-    def save_all_ITs(group_experiments : GroupAnalysis, output_folder_path):
+    def save_ITs(group_experiments : GroupAnalysis, output_folder_path):
         # This function takes all experiments (after processing) 
         # and creates output_csv files for all of them
         n_experiments = len(group_experiments.get_experiments())
         if n_experiments == 0:
             return None
         # Initialise the matrix 
-        it_matrix = []
-        file_names = []
         for i, experiment in enumerate(group_experiments.get_experiments()):
+            it_matrix = []
+            file_names = []
             for j, spheroid_file in enumerate(experiment.files):
                 IT_individual = spheroid_file.get_processed_data_IT()
                 it_matrix.append(IT_individual)
@@ -31,9 +31,61 @@ class OutputManager:
             output_path = os.path.join(output_IT_folder, output_csv)
             df.to_csv(output_path, index_label="TimePoint")
     @staticmethod
-    def save_metadata_metrics(group_experiments : GroupAnalysis, output_folder_path):
-        pass
-
+    def save_original_ITs(group_experiments : GroupAnalysis, output_folder_path):
+        # This function takes all experiments (after processing) 
+        # and creates output_csv files for all of them
+        n_experiments = len(group_experiments.get_experiments())
+        if n_experiments == 0:
+            return None
+        # Initialise the matrix 
+        for i, experiment in enumerate(group_experiments.get_experiments()):
+            it_matrix = []
+            file_names = []
+            for j, spheroid_file in enumerate(experiment.files):
+                IT_individual = spheroid_file.get_original_data_IT()
+                it_matrix.append(IT_individual)
+                file_name = spheroid_file.get_filepath()
+                file_names.append(file_name)
+            # Transpose so each column is a file
+            df = pd.DataFrame(it_matrix).T
+            df.columns = [f"File_{i}" for i in range(len(file_names))]
+            df.columns = [file_name.split("/")[-1] for file_name in file_names]
+            output_csv = "Original_ITs_experiment_n{0}.csv".format(i)
+            output_IT_folder = os.path.join(output_folder_path,"original_ITs_per_replicate")
+            if os.path.isdir(output_IT_folder) == False:
+                os.mkdir(output_IT_folder)
+            output_path = os.path.join(output_IT_folder, output_csv)
+            df.to_csv(output_path, index_label="TimePoint")
+    @staticmethod
+    def save_peak_amplitudes_metrics(group_experiments : GroupAnalysis, output_folder_path):
+        # This function saves the following keys per file in a folder named metadata_files
+        # Particularly these are the saves the method can save:
+        # # Current metadata:
+            # dict_keys(['peak_position', 'stim_start', 'stim_duration', 'stim_frequency', 
+            # 'background_subtraction_region', 'baseline', 'experiment_first_peak', 'peak_amplitude_positions', 
+            # 'peak_amplitude_values', 'exponential fitting parameters']) dict_values([257, 5.0, 2.0, 20, (0, 10), 
+            # array([0.02988434]), np.float64(0.3822367999525832), array([74]), array([1.04633804]), 
+            # {'A': np.float64(1.5705880537206165), 'tau': np.float64(119.00028560994886), 
+            # 'C': np.float64(0.22708457061563378), 't_half': np.float64(82.48471245636428)}])
+        # This method saves the following keys: keys = ['peak_amplitude_positions','peak_amplitude_values']
+        keys = ['peak_amplitude_positions','peak_amplitude_values']
+        for i, experiment in enumerate(group_experiments.get_experiments()):
+            records = []
+            for j, spheroid_file in enumerate(experiment.files):
+                meta = spheroid_file.get_metadata()
+                if keys is None:
+                    # Save all keys
+                    records.append(meta)
+                else:
+                    # Save only selected keys
+                    records.append({k: meta.get(k, None) for k in keys})
+                df = pd.DataFrame(records)
+                output_csv = "Files_Amplitudes_experiment_n{0}.csv".format(i)
+                output_IT_folder = os.path.join(output_folder_path,"amplitudes_files")
+                if os.path.isdir(output_IT_folder) == False:
+                    os.mkdir(output_IT_folder)
+                output_path = os.path.join(output_IT_folder, output_csv)
+                df.to_csv(output_path, index_label="File Number")
 
 
 if __name__ == "__main__":
@@ -55,6 +107,8 @@ if __name__ == "__main__":
 
     # Save ITs
     output_folder = r"/Users/pabloprieto/Library/CloudStorage/OneDrive-Personal/Documentos/1st_Year_PhD/Projects/NeuroStemVolt/output"
-    OutputManager.save_all_ITs(group_analysis,output_folder)
+    #OutputManager.save_ITs(group_analysis,output_folder)
+    OutputManager.save_original_ITs(group_analysis,output_folder)
+    OutputManager.save_peak_amplitudes_metrics(group_analysis,output_folder)
 
 
