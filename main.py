d840887e84e68a410bebab05f1719f51d5afb60c
Pablo <pabloprietoroca@gmail.com>
2025-06-30 14:07:49 +0100

    Added logo to the app

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from ui.neurostemvolt_ui import *
 import sys
+import os
 
 app = QApplication(sys.argv)
 wizard = QWizard()
 wizard.setWindowTitle("NeuroStemVolt Wizard")
+icon_path = os.path.join(os.path.dirname(__file__), "ui", "LogoNeuroStemVolt.png")
+app.setWindowIcon(QIcon(icon_path))
 wizard.addPage(IntroPage())
 wizard.addPage(ColorPlotPage())
 wizard.addPage(ResultsPage())
